7128684827b1808d4c7ee7f7277e984aaad5376f
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-03 13:28:40 -0600

    created add friend and delete friend methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,8 +65,51 @@ module.exports = {
   },
   // BONUS: remove a user's associated thoughts when deleted
 
-  //Post to add a new friend to a user's friend list
-  addFriend(req, res) {},
-  //delete to remove a friend from a user's friend list
-  deleteFriend(req, res) {},
+  //Add a new friend to a user's friend list
+  addFriend(req, res) {
+    User.findByIdAndUpdate(
+      { _id: req.params.userId },
+      {
+        //push to add friend to array
+        $push: {
+          friends: req.params.friendId,
+        },
+      }
+    )
+      .then((addedFriend) => {
+        if (!addedFriend) {
+          res.status(404).json({
+            message: "Cannot add friend. No user found with that Id",
+          });
+        } else {
+          res.json(addedFriend);
+        }
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  //Delete to remove a friend from a user's friend list
+  deleteFriend(req, res) {
+    //Find the user that wants to remove friend
+    User.findOne({ _id: req.params.userId }).then((userData) => {
+      //if userId doesn't exists show the message
+      if (!userData) {
+        res.status(404).json({
+          message: "No user found with that Id",
+        });
+        //if user does exist, then we can delete the friend if they exist
+      } else {
+        User.findOneAndRemove({ friends: req.params.friendId })
+          .then((removedFriend) => {
+            if (!removedFriend) {
+              res.status(404).json({
+                message: "This friend does not exist.",
+              });
+            } else {
+              res.json(removedFriend);
+            }
+          })
+          .catch((err) => res.status(500).json(err));
+      }
+    });
+  },
 };
